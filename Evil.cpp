eb470725233411e82f12c5021a5d70b915aae4bd
BlackBullet2006 <slepovapolina681@gmail.com>
2026-01-30 09:34:33 +0400

    Update and rename BaseClass.h to Evil.cpp

---
+++ Evil.cpp
@@ -0,0 +1,30 @@
+#include "Evil.h"
+#include <string>
+
+Evil::Evil(std::string name) {
+	this->name = name;
+	health = 10;
+	damage = 5;
+}
+Evil::Evil(string name, int health) : Evil(name) {
+	this->health = health;
+}
+Evil::Evil(string name, int health, int damage) : Evil(name, health) {
+	this->damage = damage;
+}
+
+void Evil::GetInfo() {
+    cout << endl;
+    cout << "Имя врага - " << name << endl;
+
+    if (health >= 0 && damage < 0) {
+        cout << "Здоровье - " << health << endl;
+    }
+    if (health < 0 && damage >= 0) {
+        cout << "Урон - " << damage << endl;
+    }
+    if (health >= 0 && damage >= 0) {
+        cout << "Здоровье - " << health << endl;
+        cout << "Урон - " << damage << endl;
+    }
+}
